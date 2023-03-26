471435f893047e460e806ce7af5dce16489648c9
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-26 18:53:34 -0400

    arreglamos la funcion sumarCadenas y vemos que pasa los 8 tests de manera correcta

--- src/suma_cadenas.js
+++ src/suma_cadenas.js
@@ -1,14 +1,25 @@
-function sumarCadenas(cadena){
-    let suma = 0;
-    for(let i =0;i<cadena.length;i++){
-        if(cadena[i]=="," || cadena[i]=="-"){
-            suma+=0;
-        }
-        else{
-            suma +=+cadena[i];
-        }
+function sumarCadenas(cadena) {
+  let suma = 0;
+  let caracter = "";
+  if (cadena[2] == "[") {
+    caracter = cadena[3];
+  }
+  for (let i = 0; i < cadena.length; i++) {
+    if (caracter == "") {
+      if (cadena[i] == "," || cadena[i] == "-") {
+        suma += 0;
+      } else {
+        suma += +cadena[i];
+      }
     }
-    return suma;
-
+    else{
+        if (cadena[i+7] == "," || cadena[i+7] == "-" || cadena[i+7] == caracter) {
+            suma += 0;
+          } else{
+            suma += +cadena[i+7];
+          }
+    }
+  }
+  return suma;
 }
 export default sumarCadenas;
